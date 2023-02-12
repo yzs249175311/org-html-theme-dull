3fe3d320e2630dc61142a48064aaf5e915ceab3c
yuanzishuai <yuanzishuai@qq.com>
2023-02-12 17:45:16 +0800

    fix 标题文字换行显示

--- src/style.ts
+++ src/style.ts
@@ -14,11 +14,11 @@ export default (function(){
 	setStyle("#content", "w-8/12 mr-auto ml-24 relative scroll-smooth")
 	setStyle("#content h1.title", "mb-12 mt-12 shadow-none text-5xl font-bold")
 
-	setStyleAll("#table-of-contents", "z-20 fixed top-8 right-4 border rounded-lg p-4 pt-0 shadow-lg center max-h-[80%] max-w-2xs hover:max-w-fit overflow-x-hidden overflow-y-auto transition-transform translate-x-[120%] xl:translate-x-0")
+	setStyleAll("#table-of-contents", "z-20 fixed top-8 right-4 border rounded-lg p-4 pt-0 shadow-lg center max-h-[80%] max-w-xs hover:max-w-2xl overflow-x-hidden overflow-y-auto transition-transform translate-x-[120%] xl:translate-x-0")
 	setStyleAll("#table-of-contents h2", "text-xl shadow-none font-bold border-b-2 py-4 mb-4 sticky top-0 bg-white text-center")
 	setStyleAll("#table-of-contents ul:not(:nth-child(1))", "list-none list-inside m-0 pl-4")
 	setStyleAll("#table-of-contents ol:not(:nth-child(1))", "list-none list-inside m-0")
-	setStyleAll("#table-of-contents li", "whitespace-nowrap")
+	setStyleAll("#table-of-contents li", "truncate")
 
 	setStyleAll("#content pre", "rounded-lg shadow-lg border-solid m-4")
 	setStyleAll("#content ul", "list-disc list-inside m-2 pl-4")
@@ -26,21 +26,21 @@ export default (function(){
 
 	setStyleAll("p", "inline-block text-left")
 
-	setStyleAll("#content h1:not(.title)", "text-5xl my-4 h-12 z-10 shadow pl-4")
-	setStyleAll("#content h2", "text-4xl my-4 h-12 z-10 shadow pl-4")
-	setStyleAll("#content h3", "text-3xl my-4 h-12 z-10 shadow  pl-4")
-	setStyleAll("#content h4", "text-2xl my-4 h-12 z-10 shadow pl-4")
-	setStyleAll("#content h5", "text-xl my-4 h-12 z-10 shadow pl-4 ")
-	setStyleAll("#content h6", "text-lg my-4 h-12 z-10 shadow  pl-4")
+	setStyleAll("#content h1:not(.title)", "text-5xl my-4 z-10 shadow pl-4")
+	setStyleAll("#content h2", "text-4xl my-4 z-10 shadow pl-4  min-h-[3rem]")
+	setStyleAll("#content h3", "text-3xl my-4 z-10 shadow  pl-4 min-h-[3rem]")
+	setStyleAll("#content h4", "text-2xl my-4 z-10 shadow pl-4  min-h-[3rem]")
+	setStyleAll("#content h5", "text-xl my-4 z-10 shadow pl-4   min-h-[3rem]")
+	setStyleAll("#content h6", "text-lg my-4 z-10 shadow  pl-4  min-h-[3rem]")
 	
 	setStyleAll("#content a", "text-blue-800 underline")
 
-	setStyleAll("h1[id^=org]", "bg-slate-50  sticky top-0 border-solid")
-	setStyleAll("h2[id^=org]", "bg-slate-50  sticky top-0 border-solid")
-	setStyleAll("h3[id^=org]", "bg-slate-50  sticky top-0 border-solid")
-	setStyleAll("h4[id^=org]", "bg-slate-50  sticky top-0 border-solid")
-	setStyleAll("h5[id^=org]", "bg-slate-50  sticky top-0 border-solid")
-	setStyleAll("h6[id^=org]", "bg-slate-50  sticky top-0 border-solid")
+	setStyleAll("h1[id^=org]", "bg-slate-50  sticky top-0 border-solid truncate hover:z-20  hover:max-h-auto hover:whitespace-normal hover:overflow-x-auto hover:text-clip pr-8")
+	setStyleAll("h2[id^=org]", "bg-slate-50  sticky top-0 border-solid truncate hover:z-20  hover:max-h-auto hover:whitespace-normal hover:overflow-x-auto hover:text-clip pr-8")
+	setStyleAll("h3[id^=org]", "bg-slate-50  sticky top-0 border-solid truncate hover:z-20  hover:max-h-auto hover:max-h-auto hover:whitespace-normal hover:overflow-x-auto hover:text-clip pr-8")
+	setStyleAll("h4[id^=org]", "bg-slate-50  sticky top-0 border-solid truncate hover:z-20  hover:max-h-auto hover:whitespace-normal hover:overflow-x-auto hover:text-clip pr-8")
+	setStyleAll("h5[id^=org]", "bg-slate-50  sticky top-0 border-solid truncate hover:z-20  hover:max-h-auto hover:whitespace-normal hover:overflow-x-auto hover:text-clip pr-8")
+	setStyleAll("h6[id^=org]", "bg-slate-50  sticky top-0 border-solid truncate hover:z-20  hover:max-h-auto hover:whitespace-normal hover:overflow-x-auto hover:text-clip pr-8")
 
 	setStyleAll("table", "p-4 m-4 table-auto")
 	setStyleAll("thead", "bg-slate-200 rounded ")
