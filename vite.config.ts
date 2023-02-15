5e5e5b6dd4574ae5e94d3c02de4ab247b98c7953
yuanzishuai <yuanzishuai@qq.com>
2023-02-15 19:37:13 +0800

    change 更改项目名，更改代码字体

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         // 入口文件名
-        entryFileNames: 'assets/org-theme-dull.js',
+        entryFileNames: 'assets/org-html-theme-dull.js',
         // 块文件名
         chunkFileNames: 'assets/[name]-[hash].js',
         // 资源文件名 css 图片等等
-        assetFileNames: 'assets/org-theme-dull.[ext]',
+        assetFileNames: 'assets/org-html-theme-dull.[ext]',
       },
     },
   },
