803b87dfaf0c086bc7b32b3a55f8d4889898c5b9
yuanzishuai <yuanzishuai@qq.com>
2023-03-04 18:47:34 +0800

    fix 代码框可以展示所有的代码属于什么代码了

--- src/layouts/preTopBlock.ts
+++ src/layouts/preTopBlock.ts
@@ -1,6 +1,8 @@
 export default function preTopBlock(){
-	let pres = document.querySelectorAll("pre")
-	pres.forEach( item => {
+	let pres:NodeListOf<HTMLPreElement> = document.querySelectorAll("pre[class^=src]")
+
+	pres.forEach( (item:HTMLPreElement) => {
+		//给显示代码框的头部增加三个圆
 		let topBlock = document.createElement("div")
 		topBlock.classList.add("top-block")
 		topBlock.appendChild(document.createElement("span"))
@@ -8,5 +10,7 @@ export default function preTopBlock(){
 		topBlock.appendChild(document.createElement("span"))
 		item.insertAdjacentElement("afterbegin",topBlock)
 		
+		//给pre添加关于它展示的是什么代码的属性
+		item.dataset.lang = item.className.match(/src-(\w+)/)![1]
 	})
 }
