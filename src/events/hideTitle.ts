77baaa1e17c2eab3fee44ff5df8b76d90bc3d2b5
yuanzishuai <yuanzishuai@qq.com>
2023-03-15 13:27:31 +0800

    fix 隐藏title导致屏幕抖动

--- src/events/hideTitle.ts
+++ src/events/hideTitle.ts
@@ -1,21 +1,22 @@
 import { defineComponent } from "../Dull";
 
+
 // 往下滚动时，隐藏title
 function hideTitle(){
 	let title:HTMLDivElement = document.querySelector("header .title")!
 	let main = document.querySelector("main")!
+	// let sTopThreshold = 
 
 	let hidden:boolean = false;
 	main.addEventListener("scroll",(e:Event)=>{
 		let sTop = (e.currentTarget as HTMLDivElement).scrollTop
-		//条件判断的值可以设置的大一点
 		if(sTop >= 200 && !hidden){
 			//设置回调,防止隐藏title后,因为内容不够，sTop的值发生改变造成屏幕抖动
 			setTimeout(()=>{
 				title.classList.add("hidden")
 			},400)
 			hidden = true;
-		}else if(sTop<200 && hidden){
+		}else if(sTop<200-(8*24) && hidden){
 			setTimeout(()=>{
 				title.classList.remove("hidden")
 			},400)
